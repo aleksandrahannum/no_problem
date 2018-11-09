42eea3a3a55f1ccb65ecaa925ed97c5b9da64d3e
Aleksandra Hannum <aleksandrahannum@gmail.com>
2018-11-09 08:48:25 +0100

    mobile menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $(".toggle_btn").click(function () {
+        $(".main_nav_list").slideToggle();
+    })
+
+    $(".main_nav_list").addClass("active_toggle_btn")
+
+
+
+
+});
